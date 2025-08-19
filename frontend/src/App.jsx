cdac8eaab7674973880876fc3887f4529863f481
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-19 21:43:23 +0800

    Setup frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-full h-full bg-amber-200'>
+        <p>Metro RealTime</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
